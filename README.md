aba2ac580857085948b5bf38981546155fbb5e41
Alexandre Segura <mex.zktk@gmail.com>
2019-10-09 10:33:06 +0200

    Improve README, add script to generate Dockerfile.

--- README.md
+++ README.md
@@ -1,14 +1,64 @@
-This repository is used to build Docker images for testing Mes Aides on CircleCI.
+This repository is used to build Docker images for testing [Mes Aides](https://github.com/betagouv/mes-aides-ui) on CircleCI.
 
 Basically, it creates Docker images with Node & Python.
 
 The images are deployed on [Docker Hub](https://hub.docker.com/r/betagouv/mes-aides-docker)
 
-Building Docker images manually
--------------------------------
+How it works
+------------
 
-For example, to build a Docker image with Node 8 & Python 3.6:
+Each folder `node{$NODE_VERSION}-python{$PYTHON_VERSION}` contains a `Dockerfile`.
+
+On Docker Hub, [Automated Builds](https://docs.docker.com/docker-hub/builds/) are configured.
+
+Each folder is configured to build the corresponding tag.
+
+![Automated Builds](https://raw.githubusercontent.com/betagouv/mes-aides-dockerfiles/master/images/automated-builds.png "Automated Builds")
+
+Generating a new Dockerfile
+---------------------------
+
+1. Create a new folder named after the tag you want to build
 
 ```
-docker build --no-cache -t betagouv/mes-aides-docker:<tag> -f node8-python3.6/Dockerfile .
+mkdir node8-python3.7
+```
+
+2. Generate a `Dockerfile` in this folder
+
+```
+LINUX_VERSION=buster NODE_VERSION=8.16.1 PYTHON_VERSION=3.7.3 bin/generate.sh > node8-python3.7/Dockerfile
+```
+
+3. Push your changes
+
+```
+git add .
+git commit -m 'Add a new Dockerfile for Node 8 & Python 3.7.'
+git push
+```
+
+Building & pushing Docker images manually
+-----------------------------------------
+
+You may want to build the Docker image on your machine, and push it manually to Docker Hub.
+
+1. Login on Docker
+
+It will ask for your Docker hub credentials. You can skip this step if you are already authenticated.
+
+```
+docker login --username=<username>
+```
+
+2. Build the Docker image (may take a while)
+
+```
+docker build --no-cache -t betagouv/mes-aides-docker:<tag> -f node8-python3.7/Dockerfile .
+```
+
+3. Push the image to Docker Hub
+
+```
+docker push betagouv/mes-aides-docker:node8-python3.7
 ```
---
+++ bin/generate.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This file is inspired by https://github.com/CircleCI-Public/dockerfile-wizard/blob/master/scripts/generate.sh
+# It prints a Dockerfile to the standard output
+#
+# Usage:
+# LINUX_VERSION=buster NODE_VERSION=8.16.1 PYTHON_VERSION=3.7.3 bin/generate.sh
+
+echo "FROM buildpack-deps:$LINUX_VERSION"
+
+echo "RUN apt-get update"
+
+if [ ! -e $NODE_VERSION ] ; then
+    echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz && \
+    tar -xzvf node-v$NODE_VERSION.tar.gz && \
+    rm node-v$NODE_VERSION.tar.gz && \
+    cd node-v$NODE_VERSION && \
+    ./configure && \
+    make -j4 && \
+    make install && \
+    cd .. && \
+    rm -r node-v$NODE_VERSION"
+fi
+
+if [ ! -e $PYTHON_VERSION ] ; then
+    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz && \
+    tar xzf Python-$PYTHON_VERSION.tgz && \
+    rm Python-$PYTHON_VERSION.tgz && \
+    cd Python-$PYTHON_VERSION && \
+    ./configure && \
+    make install"
+fi
+
+# install lsb-release, etc., for testing linux distro
+echo "RUN apt-get update && apt-get -y install lsb-release unzip"
+
+echo "ENV DISPLAY :99"
+
+echo "# install firefox
+RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.deb https://s3.amazonaws.com/circle-downloads/firefox-mozilla-build_47.0.1-0ubuntu1_amd64.deb \
+  && echo 'ef016febe5ec4eaf7d455a34579834bcde7703cb0818c80044f4d148df8473bb  /tmp/firefox.deb' | sha256sum -c \
+  && dpkg -i /tmp/firefox.deb || apt-get -f install \
+  && apt-get install -y libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3 \
+  && rm -rf /tmp/firefox.deb"
+
+echo "# install chrome
+RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+  && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || apt-get -fy install) \
+  && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
+  && sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox --no-sandbox|g' \
+       \"/opt/google/chrome/google-chrome\""
+
+echo "# install chromedriver
+RUN apt-get -y install libgconf-2-4 \
+  && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip \"http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip\" \
+  && cd /tmp \
+  && unzip chromedriver_linux64.zip \
+  && rm -rf chromedriver_linux64.zip \
+  && mv chromedriver /usr/local/bin/chromedriver \
+  && chmod +x /usr/local/bin/chromedriver"
